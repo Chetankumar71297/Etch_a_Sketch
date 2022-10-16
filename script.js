ac8454d9f6b639ab7e0a47bf3af0cbfda7d49153
Chetan kumar <chetankumar71297@gmail.com>
2022-10-16 01:03:45 -0700

    All buttons are added
    all buttons are working except clear all.

--- script.js
+++ script.js
@@ -16,17 +16,19 @@ slider.oninput = function() {
   }
 let board = document.createElement('div');
 board.style.cssText = 'display: grid; width: 300px; height: 300px; border: 1px solid black;';
+let boardEliment;
 sketchBoard();
+
 function sketchBoard() {
-board.style.gridTemplateColumns = `repeat(${boardPixel},1fr)`;
-board.style.gridTemplateRows = `repeat(${boardPixel},1fr)`;
-board.classList.add('board');
-document.getElementById('container').appendChild(board);
+    board.style.gridTemplateColumns = `repeat(${boardPixel},1fr)`;
+    board.style.gridTemplateRows = `repeat(${boardPixel},1fr)`;
+    board.classList.add('board');
+    document.getElementById('container').appendChild(board);
 
 
     for (let i = 0; i < (boardPixel*boardPixel); i++){
-        let boardEliment = document.createElement('div');
-        //boardEliment.innerText = "c";
+        boardEliment = document.createElement('div');
+        boardEliment.style.cssText = 'background-color: #fefefe;';
         boardEliment.classList.add("pixel");
         boardEliment.addEventListener('mouseover',changeColor);
         boardEliment.addEventListener('mousedown',changeColor);
@@ -38,12 +40,23 @@ document.getElementById('container').appendChild(board);
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
-let currentMode = 'sketch';
+
+let currentMode = 'Sketch'; //default mode
+ 
+function changeMode1() {
+    currentMode = 'Sketch';
+}
+function changeMode2() {
+    currentMode = 'Eraser';
+}
+function changeMode3() {
+    sketchBoard();
+}
 function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return
-        if (currentMode === 'sketch') {
+        if (currentMode === 'Sketch') {
             e.target.style.backgroundColor = penColor;
-        } else if (currentMode === 'eraser') {
+        } else if (currentMode === 'Eraser') {
             e.target.style.backgroundColor = '#fefefe';
         }
     }
